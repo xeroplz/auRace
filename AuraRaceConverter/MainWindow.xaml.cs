e47560d7ae4ec80867df71fff368fdc7fa2ce19b
xeroplz <jtjonson16@gmail.com>
2015-10-11 22:33:53 -0400

    Added More Parameters
    almost done :>

--- AuraRaceConverter/MainWindow.xaml.cs
+++ AuraRaceConverter/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace AuraRaceConverter
 {
@@ -95,25 +96,23 @@ namespace AuraRaceConverter
 				var group = race.GetAttribute("RaceDesc");
 				var tags = race.GetAttribute("StringID");
 				var gender = race.GetAttribute("Gender");
+				var range = race.GetAttribute("MeleeAttackRange");
+				var attackSpeed = race.GetAttribute("DefaultAttackSpeed");
+				var knockCount = race.GetAttribute("DefaultDownHitCount");
+				var splashRadius = race.GetAttribute("SplashRadius");
+				var splashAngle = race.GetAttribute("SplashAngle");
+				var splashDamage = race.GetAttribute("SplashDamage");
 
 				// Vehicle Type
 				var extraData = race.GetAttribute("ExtraData");
 				var vehicleType = "0";
-				/*
 				if (extraData != "")
 				{
-					XmlDocument extraDataXml = new XmlDocument();
-					extraData.Replace("&quot;", "\"");
-					extraData.Replace("&lt;", "<");
-					extraData.Replace("&gt;", ">");
-					extraDataXml.Load(extraData);
-					var extraElements = extraDataXml.GetElementsByTagName("xml");
-					foreach (XmlElement extraElement in extraElements)
+					if (extraData.Contains("vehicle type"))
 					{
-						vehicleType = (extraElement.GetAttribute("vehicle type"));
+						vehicleType = Regex.Match(extraData, "vehicle type=\"(.*)\"").Groups[1].Value;
 					}
 				}
-				*/
 
 				// Get RunSpeedFactor from raceNA.xml
 				var runSpeedFactor = "0";
@@ -128,7 +127,7 @@ namespace AuraRaceConverter
 
 				// State
 				var stateTemp = race.GetAttribute("IsGoodNPC");
-				string state = "";
+				var state = "";
 				if (stateTemp == "false")
 				{
 					state = "0x80000000";
@@ -138,18 +137,138 @@ namespace AuraRaceConverter
 					state = "0xA0000000";
 				}
 
+				// Stand
+				var stand = "0";
+				var isKnockBack = race.GetAttribute("ShovedEnable");
+				var isKnockDown = race.GetAttribute("BlowAwayEnable");
+				if ((isKnockBack == "true") && (isKnockDown == "false"))
+				{
+					stand = "0x01";
+				}
+				else if ((isKnockBack == "false") && (isKnockDown == "true"))
+				{
+					stand = "0x02";
+				}
+				else if ((isKnockBack == "true") && (isKnockDown == "true"))
+				{
+					stand = "0x03";
+				}
+
 				var invWidth = race.GetAttribute("InventorySizeX");
 				var invHeight = race.GetAttribute("InventorySizeY");
 
-				var raceText=("{ " + 
+				var attackMinBase = "0";
+				var attackMaxBase = "0";
+				var attackMinBaseMod = "0";
+				var attackMaxBaseMod = "0";
+				var injuryMinBase = "0";
+				var injuryMaxBase = "0";
+				var InjuryMinBaseMod = "0";
+				var InjuryMaxBaseMod = "0";
+				var level = "0";
+				var strength = "0";
+				var intelligence = "0";
+				var dexterity = "0";
+				var will = "0";
+				var luck = "0";
+				var life = "0";
+				var mana = "0";
+				var stamina = "0";
+				var criticalBase = "0";
+				var criticalBaseMod = "0";
+				var balanceBase = "0";
+				var balanceBaseMod = "0";
+				var defense = "0";
+				var protection = "0";
+				var sizeMin = "0";
+				var sizeMax = "0";
+				var combatPower = "0";
+				var colorHex = "0";
+				var exp = "0";
+				var goldMin = "0";
+				var goldMax = "0";
+				var elementPhysical = "0";
+				var elementLightning = "0";
+				var elementFire = "0";
+				var elementIce = "0";
+
+				// Get monster.xml values for above variables
+				var monsters = monsterXML.GetElementsByTagName("Monster");
+				foreach (XmlElement monster in monsters)
+				{
+					if (id == monster.GetAttribute("RaceID")) // If same race
+					{
+						attackMinBase = monster.GetAttribute("AttMin");
+						attackMaxBase = monster.GetAttribute("AttMax");
+
+						injuryMinBase = monster.GetAttribute("WAttMin");
+						injuryMaxBase = monster.GetAttribute("WAttMax");
+
+						level = monster.GetAttribute("Level");
+						strength = monster.GetAttribute("Strength");
+						intelligence = monster.GetAttribute("Intelligence");
+						dexterity = monster.GetAttribute("Dexterity");
+						will = monster.GetAttribute("Will");
+						luck = monster.GetAttribute("Luck");
+						life = monster.GetAttribute("Life");
+						mana = monster.GetAttribute("Mana");
+						stamina = monster.GetAttribute("Stamina");
+
+						criticalBase = monster.GetAttribute("Critical");
+						balanceBase = monster.GetAttribute("Rate");
+						defense = monster.GetAttribute("Defense");
+						protection = monster.GetAttribute("Protect");
+
+						sizeMin = monster.GetAttribute("SizeMin");
+						sizeMax = monster.GetAttribute("SizeMax");
+
+						combatPower = monster.GetAttribute("CombatPower2");
+						colorHex = monster.GetAttribute("Color_Hex");
+
+						exp = monster.GetAttribute("ParticipationExp");
+						goldMin = monster.GetAttribute("BonusMoneyMin");
+						goldMax = monster.GetAttribute("BonusMoneyMax");
+
+						elementPhysical = monster.GetAttribute("ElementPhysical");
+						elementLightning = monster.GetAttribute("ElementLightning");
+						elementFire = monster.GetAttribute("ElementFire");
+						elementIce = monster.GetAttribute("ElementIce");
+					}
+				}
+
+				// Create String
+				var raceText = ("{ " +
 					"id: " + id + ", " +
 					"name: " + inQuotes(name) + ", " +
-                    "group: " + inQuotes(group) + ", " +
-                    "tags: " + inQuotes(tags) + ", " +
+					"group: " + inQuotes(group) + ", " +
+					"tags: " + inQuotes(tags) + ", " +
 					"gender: " + gender + ", " +
-					"vehicleType: " + vehicleType + ", " + 
-					"runSpeedFactor: " + runSpeedFactor + ", " + 
-					"state: " + state + ", " + "},");
+					"vehicleType: " + vehicleType + ", " +
+					"runSpeedFactor: " + runSpeedFactor + ", " +
+					"state: " + state + ", " +
+					"invWidth: " + invWidth + ", " +
+					"invHeight: " + invHeight + ", " +
+					"attackMinBase: " + attackMinBase + ", " +
+					"attackMaxBase: " + attackMaxBase + ", " +
+					"attackMinBaseMod: " + attackMinBaseMod + ", " +
+					"attackMaxBaseMod: " + attackMaxBaseMod + ", " +
+					"injuryMinBase: " + injuryMinBase + ", " +
+					"injuryMaxBase: " + injuryMaxBase + ", " +
+					"injuryMinBaseMod: " + InjuryMinBaseMod + ", " +
+					"injuryMaxBaseMod: " + InjuryMaxBaseMod + ", " +
+					"range: " + range + ", " +
+					"attackSpeed: " + attackSpeed + ", " +
+					"knockCount: " + knockCount + ", " +
+					"criticalBase: " + criticalBase + ", " +
+					"criticalBaseMod: " + criticalBaseMod + ", " +
+					"balanceBase: " + balanceBase + ", " +
+					"balanceBaseMod: " + balanceBaseMod + ", " +
+					"splashRadius: " + splashRadius + ", " +
+					"splashAngle: " + splashAngle + ", " +
+					"splashDamage: " + splashDamage + ", " +
+					"stand: " + stand + ", " +
+					"ai: \"none\"" + ", " + // Ai none until specified
+					"},");
 
 				lines.Add(raceText);
 			}
@@ -158,7 +277,7 @@ namespace AuraRaceConverter
 			successLabel.Content = "Success!";
 		}
 
-		private string inQuotes (string text)
+		private string inQuotes(string text)
 		{
 			return ("\"") + text + ("\"");
 		}
